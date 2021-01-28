38cf31f021bb97248d6d4201e44d37ef61f4b225
doig007 <doig@alum.mit.edu>
2021-01-28 14:06:08 +0000

    Fix Docker implementation

--- app/app.py
+++ app/app.py
@@ -66,7 +66,7 @@ class CarbonIntensity():
             return {'response': returndata}, 200
 
 
-api.add_resource(Places, '/places')
+api.add_resource(CarbonIntensity, '/carbon')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
